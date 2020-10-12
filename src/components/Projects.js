4c43c0a42b1efa345b60171d3e58146259011f25
Djimovanberlo <djimovanberlo@gmail.com>
2020-10-12 16:03:29 +0200

    technically got carousel working

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,76 +1,56 @@
 import React from "react";
-import SingleProject from "./SingleProject";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
+import SingleProject from "./SingleProject";
 import { projectList } from "../constants/projectList";
 
 const Projects = () => {
   return (
-    <div>
-      <div style={{ marginLeft: 280, zIndex: 1 }}>
-        {projectList.map((project) => {
-          return (
-            <SingleProject
-              key={project.id}
-              name={project.name}
-              stack={project.stack}
-              description={project.description}
-              link={project.link}
-              github={project.github}
-            />
-          );
-        })}
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        HELO
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <>HELO</>
-        <br></br>
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        HELO
-        <br></br>
-        <>HELO</>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        HELO
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <>HELO</>
-        <br></br>
-      </div>
+    <div className="content">
+      <CarouselProvider
+        naturalSlideWidth={50}
+        naturalSlideHeight={50}
+        totalSlides={projectList.length}
+      >
+        <Slider>
+          {projectList.map((project) => {
+            return (
+              // <Slide index={project.id}>
+              <SingleProject
+                key={project.id}
+                name={project.name}
+                stack={project.stack}
+                description={project.description}
+                link={project.link}
+                github={project.github}
+              />
+              // </Slide>
+            );
+          })}
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
+      {/* <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>I am the first Slide.</Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider> */}
     </div>
   );
 };
--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,39 +1,45 @@
 import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const SingleProject = (props) => {
   return (
-    <div>
-      <div style={{ marginLeft: 280 }}>
-        <div>{props.name}</div>
-        <div>
-          {props.github ? (
-            <a href={props.github} target="_blank">
-              <img src={require("../img/icons/Github.png")} />
-            </a>
-          ) : null}
-        </div>
-        <div>
-          {props.stack.map((tech, index) => {
-            return tech ? (
-              <img
-                src={require(`../img/icons/${tech}.png`)}
-                style={{ resizeMode: "cover", width: 35, height: 35 }}
-                key={index}
-              />
-            ) : // TO DO: Add the tech as mouseover tooltip
-            null;
-          })}
-        </div>
-        <div>{props.description ? props.description : null}</div>
-        <div>
-          {props.link ? (
-            <a href={props.link} target="_blank" style={{ cursor: "pointer" }}>
-              Link to project
-            </a>
-          ) : null}
-        </div>
+    <Slide index={props.id}>
+      <div>{props.name}</div>
+      <div>
+        {props.github ? (
+          <a href={props.github} target="_blank">
+            <img src={require("../img/icons/Github.png")} />
+          </a>
+        ) : null}
       </div>
-    </div>
+      <div>
+        {props.stack.map((tech, index) => {
+          return tech ? (
+            <img
+              src={require(`../img/icons/${tech}.png`)}
+              style={{ resizeMode: "cover", width: 35, height: 35 }}
+              key={index}
+            />
+          ) : // TO DO: Add the tech as mouseover tooltip
+          null;
+        })}
+      </div>
+      <div>{props.description ? props.description : null}</div>
+      <div>
+        {props.link ? (
+          <a href={props.link} target="_blank" style={{ cursor: "pointer" }}>
+            Link to project
+          </a>
+        ) : null}
+      </div>
+    </Slide>
   );
 };
 
