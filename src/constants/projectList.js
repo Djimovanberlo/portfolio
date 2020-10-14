9b72f540255d6aa6bdabd71c6c63817574d6bbc1
Djimovanberlo <djimovanberlo@gmail.com>
2020-10-14 14:39:18 +0200

    commit before start dev

--- src/constants/projectList.js
+++ src/constants/projectList.js
@@ -16,9 +16,16 @@ export const projectList = [
       "Soaring flocks is my portfolio project for Codaisseur. I built a full stack game, where players can register/login. Every night, players get awarded resources, which they can then freely trade or invest into more resources. Soaring Flocks also contains a chatbox, built with GraphQL subscriptions.",
     link: "http://soaring-flocks.netlify.app",
     github: "https://github.com/Djimovanberlo/soaring-flocks-client",
+  },  {
+    id: 2,
+    name: "Fake ID Generator",
+    stack: ["React", "Redux", "CSS"],
+    description: `Fake ID generator was our group project during the Codaisseur Academy. Within the theme "travel", we wanted to use a face-detection API, where users can add gorgeous and not so gorgeous features to their face. Built in about three days.`,
+    link: "https://relaxed-heisenberg-4577a7.netlify.app/",
+    github: "https://github.com/wmw123/fake-id-generator",
   },
   {
-    id: 2,
+    id: 3,
     name: "Groceries App",
     stack: [
       "TypeScript",
@@ -34,16 +41,7 @@ export const projectList = [
       "Work in progress. This app helps you create an efficient shopping route in your grocery store. This to increase efficiency (and for me, so I don't get lost). Also helps reduce crowds for COVID. Administrators can add grocery stores and products to the database. It is a full stack app to learn TypeScript and TypeGraphQL.",
     link: null,
     github: null,
-  },
-  {
-    id: 3,
-    name: "Fake ID Generator",
-    stack: ["React", "Redux", "CSS"],
-    description: `Fake ID generator was our group project during the Codaisseur Academy. Within the theme "travel", we wanted to use a face-detection API, where users can add gorgeous and not so gorgeous features to their face. Built in about three days.`,
-    link: "https://relaxed-heisenberg-4577a7.netlify.app/",
-    github: "https://github.com/wmw123/fake-id-generator",
-  },
-  {
+  },{}
     id: 4,
     name: "Student Platform",
     stack: [null],
@@ -55,7 +53,7 @@ export const projectList = [
   {
     id: 5,
     name: "Talent Calculator",
-    stack: ["React", "Redux"],
+    stack: ["React", "Redux", "CSS"],
     description:
       "I'd like to rebuild World of Warcraft classic's talent calculator, or even build a calculator-maker. It's on the list.",
     link: null,
@@ -64,7 +62,7 @@ export const projectList = [
   {
     id: 6,
     name: "Portfolio",
-    stack: ["React"],
+    stack: ["React", "CSS"],
     description:
       "As you might have guessed this is my portfolio site. For now in React Native, since I wanted to work with a bit.",
     link: null,
