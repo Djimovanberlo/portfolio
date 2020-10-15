9b2927871fd0db99da250cf2642fccfa76961570
Djimovanberlo <djimovanberlo@gmail.com>
2020-10-15 14:07:59 +0200

    changed some project descriptions

--- src/constants/projectList.js
+++ src/constants/projectList.js
@@ -13,7 +13,7 @@ export const projectList = [
       "Sequelize",
     ],
     description:
-      "Soaring flocks is my portfolio project for Codaisseur. I built a full stack game, where players can register/login. Every night, players get awarded resources, which they can then freely trade or invest into more resources. Soaring Flocks also contains a chatbox, built with GraphQL subscriptions.",
+      "Soaring flocks is my portfolio project for Codaisseur. Players take on the role of capitalist birds, and trade and invest their resources. I designed the gameplay in a couple of days, after which the programming started. It is a full stack app, where players can register/login. Every night, players get awarded resources, which they can then freely trade or invest in order to earn more resources. Soaring Flocks also contains a chatbox, built with GraphQL subscriptions. The most challenging part of this project was learning GraphQL from scratch while time was short.",
     link: "http://soaring-flocks.netlify.app",
     github: "https://github.com/Djimovanberlo/soaring-flocks-client",
   },
@@ -21,7 +21,7 @@ export const projectList = [
     id: 2,
     name: "Fake ID Generator",
     stack: ["React", "Redux", "CSS"],
-    description: `Fake ID generator was our group project during the Codaisseur Academy. Within the theme "travel", we wanted to use a face-detection API, where users can add gorgeous and not so gorgeous features to their face. Built in about three days.`,
+    description: `Fake ID generator was our group project during the Codaisseur Academy. Within the theme "travel", we wanted to use a face-detection API, where users can add gorgeous and not so gorgeous features to their face. Working as a group went very smoothly, while we had the opportunity to be both productive and playful. This project was built in about four days, including concept and design.`,
     link: "https://relaxed-heisenberg-4577a7.netlify.app/",
     github: "https://github.com/wmw123/fake-id-generator",
   },
@@ -36,10 +36,9 @@ export const projectList = [
       "Redux",
       "Bootstrap",
       "GraphQL",
-      "Sequelize",
     ],
     description:
-      "Work in progress. This app helps you create an efficient shopping route in your grocery store. This to increase efficiency (and for me, so I don't get lost). Also helps reduce crowds for COVID. Administrators can add grocery stores and products to the database. It is a full stack app to learn TypeScript and TypeGraphQL.",
+      "Work in progress. This app helps you create an efficient shopping route in your grocery store. This to increase efficiency (and for me, so I don't get lost). Also helps reduce crowds in supermarkets, so stupid COVID doesn't get its way. In this app, administrators can add grocery stores and products to the database. It is a full stack app to expand my skills while learning TypeScript and TypeGraphQL. When it's done, I will be using it every week to optimize my shopping.",
     link: null,
     github: null,
   },
@@ -48,7 +47,7 @@ export const projectList = [
     name: "Student Platform",
     stack: [null],
     description:
-      "I would like to build a simple platform that connects school- and college students with peers around the world. I feel it's better to learn language and culture by interacting, not exclusively reading books:)",
+      "I would like to build a simple platform that connects school- and college students with peers around the world. Students could chat, post / comment and perhaps make assignments together. I feel it's better to learn language and culture by interacting, not exclusively reading books :). The concept / design for this has not yet been developed.",
     link: null,
     github: null,
   },
@@ -57,7 +56,7 @@ export const projectList = [
     name: "Talent Calculator",
     stack: ["React", "Redux", "CSS"],
     description:
-      "I'd like to rebuild World of Warcraft classic's talent calculator, or even build a calculator-maker. It's on the list.",
+      "I'd like to rebuild World of Warcraft classic's talent calculator, or even build a calculator-maker. I like to fantasize about game- and character design, and rebuilding this would be a very doable and fun challange.",
     link: null,
     github: null,
   },
@@ -66,7 +65,7 @@ export const projectList = [
     name: "Portfolio",
     stack: ["React", "CSS"],
     description:
-      "As you might have guessed this is my portfolio site. For now in React Native, since I wanted to work with a bit.",
+      "As you might have guessed this is my portfolio site. I started building it in React Native, since I wanted to work with a bit. About halfway through however, I switched to normal React because I wanted to be able to show a normal website. React Native was fun and very easy to learn.",
     link: null,
     github: "https://github.com/Djimovanberlo/portfolio",
   },
